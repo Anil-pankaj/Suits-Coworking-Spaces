3e311d8edf6e887466e5e9c4bd70c59d23abb015
Anil Pankaj <110874385+Anil-pankaj@users.noreply.github.com>
2024-08-03 14:17:16 +0530

    Add files via upload

---
+++ CLIENT/src/routes/homePage/homePage.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Discover your dream home with our exclusive property listings. Whether 
+          you're buying or selling, we offer a seamless experience with expert 
+          guidance every step of the way. Explore prime locations, detailed property 
+          insights, and competitive pricing to find the perfect match for your needs.
+          Start your journey today and turn your real estate dreams into reality with us.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
